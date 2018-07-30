b2920e8a8a6a774dc67e7b0382bc638447e4cb72
Sivakanth Rebba <rm.sivakanth@outlook.com>
2018-07-30 01:18:47 -0700

    Added UDP support in NTTTCP test

--- Testscripts/Linux/perf_ntttcp.sh
+++ Testscripts/Linux/perf_ntttcp.sh
@@ -22,17 +22,35 @@ ICA_TESTABORTED="TestAborted"           # Error during the setup of the test
 ICA_TESTFAILED="TestFailed"             # Error occurred during the test
 touch ./ntttcpTest.log
 
-log_folder="ntttcp-test-logs"
+LogMsg()
+{
+	echo `date "+%b %d %Y %T"` : "${1}"    # Add the time stamp to the log message
+	echo "${1}" >> ./ntttcpTest.log
+}
+UpdateTestState()
+{
+	echo "${1}" > ./state.txt
+}
+
+if [ -e ${CONSTANTS_FILE} ]; then
+	source ${CONSTANTS_FILE}
+else
+	errMsg="Error: missing ${CONSTANTS_FILE} file"
+	LogMsg "${errMsg}"
+	UpdateTestState $ICA_TESTABORTED
+	exit 10
+fi
+
+log_folder="ntttcp-${testType}-test-logs"
 max_server_threads=64
 
 InstallNTTTCP()
 {
 	DISTRO=`grep -ihs "ubuntu\|Suse\|Fedora\|Debian\|CentOS\|Red Hat Enterprise Linux\|clear-linux-os" /etc/{issue,*release,*version} /usr/lib/os-release`
-
+    LogMsg "Configuring ${1} for ntttcp ${testType} test..."
 	if [[ $DISTRO =~ "Ubuntu" ]];
 	then
 		LogMsg "Detected UBUNTU"
-		LogMsg "Configuring ${1} for ntttcp test..."
 		ssh ${1} "until dpkg --force-all --configure -a; sleep 10; do echo 'Trying again...'; done"
 		ssh ${1} "apt-get update"
 		ssh ${1} "apt-get -y install libaio1 sysstat git bc make gcc dstat psmisc"
@@ -42,7 +60,6 @@ InstallNTTTCP()
 		ssh ${1} "rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm"
 		ssh ${1} "yum -y --nogpgcheck install libaio1 sysstat git bc make gcc dstat psmisc"
 		ssh ${1} "yum -y --nogpgcheck install gcc-c++"
-
 		ssh ${1} "wget http://ftp.heanet.ie/mirrors/gnu/libc/glibc-2.14.1.tar.gz"
 		ssh ${1} "tar xvf glibc-2.14.1.tar.gz"
 		ssh ${1} "mv glibc-2.14.1 glibc-2.14 && cd glibc-2.14 && mkdir build && cd build && ../configure --prefix=/opt/glibc-2.14 && make && make install && export LD_LIBRARY_PATH=/opt/glibc-2.14/lib:$LD_LIBRARY_PATH"
@@ -112,11 +129,6 @@ InstallNTTTCP()
 	ssh ${1} "iptables -F"
 	ssh ${1} "sysctl -w net.core.rmem_max=67108864; sysctl -w net.core.rmem_default=67108864; sysctl -w net.core.wmem_default=67108864; sysctl -w net.core.wmem_max=67108864"
 }
-LogMsg()
-{
-	echo `date "+%b %d %Y %T"` : "${1}"    # Add the time stamp to the log message
-	echo "${1}" >> ./ntttcpTest.log
-}
 
 runNtttcp()
 {
@@ -125,7 +137,7 @@ runNtttcp()
 	ssh ${client} "mkdir -p $log_folder"
 	LogMsg "$server $client $testConnections"
 	result_file="${log_folder}/report.csv"
-	echo "TestConnections,ThroughputInGbps,Cycles/Byte,AvgLatency" > $result_file
+	echo "TestConnections,TxThroughputInGbps,RxThroughputInGbps,TxCycles/Byte,RxCycles/Byte,AvgLatency" > $result_file
 
 	for current_test_threads in $testConnections; do
 		if [[ $current_test_threads -lt $max_server_threads ]];
@@ -136,76 +148,94 @@ runNtttcp()
 			num_threads_P=$max_server_threads
 			num_threads_n=$(($current_test_threads/$num_threads_P))
 		fi
-		ntttcp_log_prefix="ntttcp-sender-p"
-		lagscope_log_prefix="lagscope-ntttcp-p"
-		ntttcp_log_file="$log_folder/${ntttcp_log_prefix}${num_threads_P}X${num_threads_n}.log"
-		lagscope_log_file="$log_folder/${lagscope_log_prefix}${num_threads_P}X${num_threads_n}.log"
+		
 		throughput=0
 		cyclesperbytes=0
 		avglatency=0
 
-		LogMsg "==================================="
-		LogMsg "Running Test: $current_test_threads connections : $num_threads_P X $num_threads_n"
-		LogMsg "==================================="
-		ssh ${server} "for i in {1..$testDuration}; do ss -ta | grep ESTA | grep -v ssh | wc -l >> ./$log_folder/tcp-connections-p${num_threads_P}X${num_threads_n}.log; sleep 1; done" &
+		LogMsg "============================================="
+		LogMsg "Running ${testType} Test: $current_test_threads connections : $num_threads_P X $num_threads_n"
+		LogMsg "============================================="
+		
+		if [[ $testType == "udp" ]] || [[ $testType == "UDP" ]] || [[ $testType == "Udp" ]];
+		then
+			LogMsg "Test running in ${testType} mode"
+			txlogFile="${testType}-${bufferLength}-sender-p${num_threads_P}X${num_threads_n}.log"
+			rxlogFile="${testType}-${bufferLength}-receiver-p${num_threads_P}X${num_threads_n}.log"
+            serverNTTTCPCmd="ulimit -n 204800 && ntttcp -P ${num_threads_P} -t ${testDuration} -e -u -b ${bufferLength}"
+			clientNTTTCPCmd="ntttcp -s${server} -P ${num_threads_P} -n ${num_threads_n} -t ${testDuration} -u -b ${bufferLength}"
+		else
+			LogMsg "Test running in ${testType} mode"
+			txlogFile="${testType}-sender-p${num_threads_P}X${num_threads_n}.log"
+			rxlogFile="${testType}-receiver-p${num_threads_P}X${num_threads_n}.log"
+            serverNTTTCPCmd="ulimit -n 204800 && ntttcp -P ${num_threads_P} -t ${testDuration} -e"
+            clientNTTTCPCmd="ntttcp -s${server} -P ${num_threads_P} -n ${num_threads_n} -t ${testDuration}"
+            ssh ${server} "for i in {1..$testDuration}; do ss -ta | grep ESTA | grep -v ssh | wc -l >> ./$log_folder/tcp-connections-p${num_threads_P}X${num_threads_n}.log; sleep 1; done" &
+		fi
+        
+        tx_ntttcp_log_file="$log_folder/ntttcp-${txlogFile}"
+        tx_lagscope_log_file="$log_folder/lagscope-${txlogFile}"
+        rx_ntttcp_log_file="$log_folder/ntttcp-${rxlogFile}"
 
-		ssh ${server} "pkill -f ntttcp"
-		ssh ${server} "ulimit -n 204800 && ntttcp -P $num_threads_P -t ${testDuration} -e > ./$log_folder/ntttcp-receiver-p${num_threads_P}X${num_threads_n}.log" &
+        ssh ${server} "pkill -f ntttcp"
+        LogMsg "$serverNTTTCPCmd > ./$log_folder/ntttcp-${rxlogFile}"
+        ssh ${server} "${serverNTTTCPCmd}" > "./$log_folder/ntttcp-${rxlogFile}" &
 
 		ssh ${server} "pkill -f lagscope"
-		ssh ${server} "lagscope -r" &
+        ssh ${server} "lagscope -r" &
 
 		ssh ${server} "pkill -f dstat"
-		ssh ${server} "dstat -dam > ./$log_folder/dstat-receiver-p${num_threads_P}X${num_threads_n}.log" &
+		ssh ${server} "dstat -dam" > "./$log_folder/dstat-${rxlogFile}" &
 
 		ssh ${server} "pkill -f mpstat"
-		ssh ${server} "mpstat -P ALL 1 ${testDuration} > ./$log_folder/mpstat-receiver-p${num_threads_P}X${num_threads_n}.log" &
+		ssh ${server} "mpstat -P ALL 1 ${testDuration}" > "./$log_folder/mpstat-${rxlogFile}" &
 
 		ulimit -n 204800
 		sleep 2
-		sar -n DEV 1 ${testDuration} > "./$log_folder/sar-sender-p${num_threads_P}X${num_threads_n}.log" &
-		dstat -dam > "./$log_folder/dstat-sender-p${num_threads_P}X${num_threads_n}.log" &
-		mpstat -P ALL 1 ${testDuration} > "./$log_folder/mpstat-sender-p${num_threads_P}X${num_threads_n}.log" &
-		lagscope -s${server} -t ${testDuration} -V > "./$log_folder/lagscope-ntttcp-p${num_threads_P}X${num_threads_n}.log" &
-		ntttcp -s${server} -P $num_threads_P -n $num_threads_n -t ${testDuration}  > "./$log_folder/ntttcp-sender-p${num_threads_P}X${num_threads_n}.log"
+		sar -n DEV 1 ${testDuration} > "./$log_folder/sar-${txlogFile}" &
+		dstat -dam > "./$log_folder/dstat-${txlogFile}" &
+		mpstat -P ALL 1 ${testDuration} > "./$log_folder/mpstat-${txlogFile}" &
+		lagscope -s${server} -t ${testDuration} -V > "./$log_folder/lagscope-${txlogFile}" &
+        LogMsg "${clientNTTTCPCmd} > ./${log_folder}/ntttcp-${txlogFile}"
+        $clientNTTTCPCmd > "./${log_folder}/ntttcp-${txlogFile}"
 
 		LogMsg "Parsing results for $current_test_threads connections"
-		throughput=$(cat $ntttcp_log_file | grep throughput | tail -1 | tr ":" " " | awk '{ print $NF }')
-		if [[ $throughput =~ "Gbps" ]];
+        sleep 10
+		txThroughput=$(cat $tx_ntttcp_log_file | grep throughput | tail -1 | tr ":" " " | awk '{ print $NF }')
+		if [[ $txThroughput =~ "Gbps" ]];
 		then
-			throughput=$(echo $throughput | sed 's/Gbps//')
-		elif [[ $throughput =~ "Mbps" ]];
+			txThroughput=$(echo $txThroughput | sed 's/Gbps//')
+		elif [[ $txThroughput =~ "Mbps" ]];
 		then
-			$(echo "scale=5; $throughput/1000" | bc)
+			$(echo "scale=5; $txThroughput/1000" | bc)
 		else
-			LogMsg "throughput in $throughput"
+			LogMsg "throughput in $txThroughput"
 		fi
 
-		cyclesperbytes=$(cat $ntttcp_log_file | grep cycles/byte | tr ":" " " | awk '{ print $NF }')
-		avglatency=$(cat $lagscope_log_file | grep Average | sed 's/.* //' | sed 's/us//')
-		LogMsg "Throughput in Gbps: $throughput"
-		LogMsg "Cycles/Byte : $cyclesperbytes"
-		LogMsg "AvgLaentcy in us : $avglatency"
-		echo "$current_test_threads,$throughput,$cyclesperbytes,$avglatency" >> $result_file
+        rxThroughput=$(cat $rx_ntttcp_log_file | grep throughput | tail -1 | tr ":" " " | awk '{ print $NF }')
+		if [[ $rxThroughput =~ "Gbps" ]];
+		then
+			rxThroughput=$(echo $rxThroughput | sed 's/Gbps//')
+		elif [[ $rxThroughput =~ "Mbps" ]];
+		then
+			$(echo "scale=5; $rxThroughput/1000" | bc)
+		else
+			LogMsg "throughput in $rxThroughput"
+		fi
+
+		txCyclesperbytes=$(cat $tx_ntttcp_log_file | grep cycles/byte | tr ":" " " | awk '{ print $NF }')
+		Avglatency=$(cat $tx_lagscope_log_file | grep Average | sed 's/.* //' | sed 's/us//')
+        rxCyclesperbytes=$(cat $rx_ntttcp_log_file | grep cycles/byte | tr ":" " " | awk '{ print $NF }')
+	
+		LogMsg "Throughput in Gbps : TX : $txThroughput : RX : $rxThroughput"
+		LogMsg "Cycles/Byte : TX: $txCyclesperbytes : RX: $rxCyclesperbytes"
+		LogMsg "AvgLaentcy in us : TX : $Avglatency "
+		echo "$current_test_threads,$txThroughput,$rxThroughput,$txCyclesperbytes,$rxCyclesperbytes,$Avglatency" >> $result_file
 		LogMsg "current test finished. wait for next one... "
 		i=$(($i + 1))
 		sleep 5
 	done
 }
-
-UpdateTestState()
-{
-	echo "${1}" > ./state.txt
-}
-
-if [ -e ${CONSTANTS_FILE} ]; then
-	source ${CONSTANTS_FILE}
-else
-	errMsg="Error: missing ${CONSTANTS_FILE} file"
-	LogMsg "${errMsg}"
-	UpdateTestState $ICA_TESTABORTED
-	exit 10
-fi
 
 if [ ! ${server} ]; then
 	errMsg="Please add/provide value for server in constants.sh. server=<server ip>"
@@ -245,10 +275,10 @@ InstallNTTTCP ${client}
 LogMsg "Configuring server ${server}..."
 InstallNTTTCP ${server}
 
-LogMsg "Now running NTTTCP test"
+LogMsg "Now running NTTTCP ${testType} test"
 runNtttcp
 
 column -s, -t $result_file > ./$log_folder/report.log
-ssh root@${client} "cp ntttcp-test-logs/* ."
+ssh root@${client} "cp ntttcp-${testType}-test-logs/* ."
 cat report.log
 UpdateTestState ICA_TESTCOMPLETED
